63da20c139f575664a6b33e414ef5de9b32b9551
Lunidra <Keymcc@hotmail.co.uk>
2026-05-14 01:51:21 +0100

    Final Fix for tonight, GammaUtil improvements/ Accessor

--- src/client/java/luni/ether/feature/module/utils/GammaUtil.java
+++ src/client/java/luni/ether/feature/module/utils/GammaUtil.java
@@ -1,50 +1,33 @@
 package luni.ether.feature.module.utils;
 
+import luni.ether.mixin.OptionInstanceAccessor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.OptionInstance;
 
-import java.lang.reflect.Field;
-
+@SuppressWarnings("unchecked")
 public class GammaUtil {
-
-    private static Field valueField;
-
-    static {
-        try {
-            valueField = OptionInstance.class.getDeclaredField("value");
-            valueField.setAccessible(true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void setGamma(double value) {
 
-        try {
+        Minecraft mc =
+                Minecraft.getInstance();
 
-            Minecraft mc = Minecraft.getInstance();
-            OptionInstance<Double> gamma = mc.options.gamma();
+        OptionInstance<Double> gamma =
+                mc.options.gamma();
 
-            valueField.set(gamma, value);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        ((OptionInstanceAccessor<Double>) (Object) gamma)
+                .setValue(value);
     }
 
     public static double getGamma() {
 
-        try {
+        Minecraft mc =
+                Minecraft.getInstance();
 
-            Minecraft mc = Minecraft.getInstance();
-            OptionInstance<Double> gamma = mc.options.gamma();
+        OptionInstance<Double> gamma =
+                mc.options.gamma();
 
-            return (double) valueField.get(gamma);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return 1.0;
+        return ((OptionInstanceAccessor<Double>) (Object) gamma)
+                .getValue();
     }
 }
